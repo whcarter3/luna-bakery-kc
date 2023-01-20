3b91aefd04c4eef98e9e3b6ee46aaea91ae1c3a8
William Carter <whcarter3@users.noreply.github.com>
2023-01-19 21:25:36 -0600

    changes form server to formspree, changes copy in process section

--- components/form.tsx
+++ components/form.tsx
@@ -1,95 +1,20 @@
-import { useState } from 'react';
-
-interface FormErrors {
-  email: boolean;
-  fullname: boolean;
-  subject: boolean;
-  message: boolean;
-}
+import { useForm, ValidationError } from '@formspree/react';
 
 export default function Form(): JSX.Element {
-  const [fullname, setFullname] = useState('');
-  const [email, setEmail] = useState('');
-  const [subject, setSubject] = useState('');
-  const [message, setMessage] = useState('');
-  const [errors, setErrors] = useState({} as FormErrors);
-  const [sendButton, setSendButton] = useState('Send');
-  const [showSuccess, setShowSuccess] = useState(false);
-  const [showError, setShowError] = useState(false);
+  const [state, handleSubmit] = useForm('xjvdkpky');
 
-  const handleValidation = (): boolean => {
-    let tempErrors = {} as FormErrors;
-    let isValidForm = true;
-
-    if (fullname.length <= 0) {
-      isValidForm = false;
-      tempErrors['fullname'] = true;
-    }
-
-    if (email.length <= 0) {
-      isValidForm = false;
-      tempErrors['email'] = true;
-    }
-
-    if (subject.length <= 0) {
-      isValidForm = false;
-      tempErrors['subject'] = true;
-    }
-
-    if (message.length <= 0) {
-      isValidForm = false;
-      tempErrors['message'] = true;
-    }
-
-    setErrors({ ...tempErrors });
-    return isValidForm;
-  };
-
-  const clearForm = (): void => {
-    setFullname('');
-    setEmail('');
-    setSubject('');
-    setMessage('');
-  };
-
-  const handleSubmit = async (
-    e: React.FormEvent<HTMLFormElement>
-  ) => {
-    e.preventDefault();
-
-    let isValidForm = handleValidation();
-    if (isValidForm) {
-      setSendButton('Sending...');
-
-      const res = await fetch('/api/sendgrid', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email: email,
-          fullname: fullname,
-          subject: subject,
-          message: message,
-        }),
-      });
-
-      const { error } = await res.json();
-      if (error) {
-        console.log(error);
-        setShowSuccess(false);
-        setShowError(true);
-        setSendButton('Send');
-        return;
-      }
-      setShowSuccess(true);
-      setShowError(false);
-      setSendButton('Send');
-      clearForm();
-
-      console.log(fullname, email, subject, message);
-    }
-  };
+  if (state.succeeded)
+    return (
+      <div className="w-full md:w-auto md:min-w-[700px] rounded-lg shadow-xl flex flex-col px-8 py-8 bg-white text-primary">
+        <h1 className="text-2xl font-bold dark:text-primary">
+          Message sent!
+        </h1>
+        <p className="mt-4 dark:text-primary">
+          Thank you for your message. I will get back to you as soon
+          as possible.
+        </p>
+      </div>
+    );
 
   return (
     <form
@@ -110,16 +35,14 @@ export default function Form(): JSX.Element {
       <input
         type="text"
         name="fullname"
-        value={fullname}
-        onChange={(e) => setFullname(e.target.value)}
         className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-secondary text-primary"
+        required
       />
-
-      {errors['fullname'] && (
-        <p className="text-red-500 text-sm mt-2">
-          Full name is required
-        </p>
-      )}
+      <ValidationError
+        prefix="Fullname"
+        field="fullname"
+        errors={state.errors}
+      />
 
       <label
         htmlFor="email"
@@ -130,16 +53,15 @@ export default function Form(): JSX.Element {
       <input
         type="email"
         name="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
         className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-secondary text-primary"
+        required
       />
 
-      {errors['email'] && (
-        <p className="text-red-500 text-sm mt-2">
-          E-mail is required
-        </p>
-      )}
+      <ValidationError
+        prefix="Email"
+        field="email"
+        errors={state.errors}
+      />
 
       <label
         htmlFor="subject"
@@ -150,16 +72,15 @@ export default function Form(): JSX.Element {
       <input
         type="text"
         name="subject"
-        value={subject}
-        onChange={(e) => setSubject(e.target.value)}
         className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-secondary text-primary"
+        required
       />
 
-      {errors['subject'] && (
-        <p className="text-red-500 text-sm mt-2">
-          Subject is required
-        </p>
-      )}
+      <ValidationError
+        prefix="Subject"
+        field="subject"
+        errors={state.errors}
+      />
 
       <label
         htmlFor="message"
@@ -169,32 +90,24 @@ export default function Form(): JSX.Element {
       </label>
       <textarea
         name="message"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
         className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-secondary text-primary"
+        required
       ></textarea>
 
-      {errors['message'] && (
-        <p className="text-red-500 text-sm mt-2">
-          Message is required
-        </p>
-      )}
+      <ValidationError
+        prefix="Message"
+        field="message"
+        errors={state.errors}
+      />
 
       <div className="flex flex-row items-center justify-start">
-        <button className="px-10 mt-8 py-2 bg-primary text-white rounded-md text-lg flex flex-row items-center">
-          {sendButton}
+        <button
+          className="px-10 mt-8 py-2 bg-primary text-white rounded-md text-lg flex flex-row items-center"
+          type="submit"
+          disabled={state.submitting}
+        >
+          Send
         </button>
-
-        {showSuccess && (
-          <p className="text-green-500 text-sm mt-2 ml-4">
-            Message sent successfully
-          </p>
-        )}
-        {showError && (
-          <p className="text-red-500 text-sm mt-2 ml-4">
-            Error sending message
-          </p>
-        )}
       </div>
     </form>
   );
